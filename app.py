7f8d524eb1aaf418bb623eb32fccb9dd2a0d1e82
TeresaComputer <tcpaixao-costa@myseneca.ca>
2022-01-20 11:08:47 -0500

    Added figure and dropdown

--- app.py
+++ app.py
@@ -5,38 +5,75 @@ import pandas as pd
 import yfinance as yf
 from dash import dash_table
 from dash.dash_table import DataTable, FormatTemplate
-import pandas_datareader 
 from pandas_datareader import data as pdr
+from dash.dependencies import Output, Input
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
 
-stocks = ["PPL", "SPY"]
-yahooStocks = 'PPL SPY'
+import plotly.express as px
 
-yf.pdr_override() # hijack data from yfinance
-pd.options.display.float_format = '{:,.2f}'.format # makes every float in dataframe 2 digits
+
+stocks_test = ["PPL", "SPY", "ENB", "AQN"]
+yahooStocks = 'PPL SPY ENB AQN'
+
+yf.pdr_override()  # hijack data from yfinance
+# makes every float in dataframe 2 digits
+pd.options.display.float_format = '{:,.2f}'.format
 
 # get data
-mydata = pdr.get_data_yahoo( yahooStocks, start="2022-01-19", end="2022-01-20")
-df = pd.DataFrame(mydata['Open']) # get recent quote
+mydata = pdr.get_data_yahoo(yahooStocks, start="2022-01-19", end="2022-01-20")
+df = pd.DataFrame(mydata['Open'])  # get recent quote
 
 # format data
-for stock in stocks:
+for stock in stocks_test:
     df[stock] = df[stock].map(lambda x: '{0:.2f}'.format(x))
 
 df_t = df.T
-df_t.columns = ['Quote'] 
-df_t.insert(0, "Ticket", stocks, True)
+df_t.columns = ['Quote']
+df_t.insert(0, "Ticket", stocks_test, True)
 
 print(df_t)
 # dash app
+# X = deque(maxlen = 20)
+# X.append(1)
+
+# Y = deque(maxlen = 20)
+# Y.append(1)
+df_test = px.data.stocks()
 app = dash.Dash(__name__)
 
-# money = FormatTemplate.money(2)
+# # money = FormatTemplate.money(2)
 
-app.layout = dash_table.DataTable(
-    id='table',
-    columns=[{"name": i, "id": i} for i in df_t.columns],
-    data=df_t.to_dict('records'),
+app.layout = html.Div(
+    [
+        dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in df_t.columns],
+            data=df_t.to_dict('records')),
+        dcc.Dropdown(
+            id="ticker",
+            options=[{"label": x, "value": x} for x in df_test.columns[1:]],
+            value=df_test.columns[1],
+            clearable=False,
+        ),
+         dcc.Dropdown(
+            id="second_ticker",
+            options=[{"label": x, "value": x} for x in df_test.columns[1:]],
+            value=df_test.columns[1],
+            clearable=False,
+        ),
+        dcc.Graph(id="time-series-chart"),
+    ]
 )
+
+@app.callback(
+    Output("time-series-chart", "figure"), 
+    [Input("ticker", "value")])
+def display_time_series(ticker):
+    fig = px.line(df_test, x='date', y=ticker)
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
