3ccdba4e78807771328dc69faf2b8ec89a8cb433
TeresaComputer <tcpaixao-costa@myseneca.ca>
2022-01-20 14:17:11 -0500

    Changes layout, input data, new functions

--- app.py
+++ app.py
@@ -11,69 +11,85 @@ import plotly
 import random
 import plotly.graph_objs as go
 from collections import deque
-
+from datetime import date
 import plotly.express as px
-
+import datetime
 
 stocks_test = ["PPL", "SPY", "ENB", "AQN"]
 yahooStocks = 'PPL SPY ENB AQN'
 
+# dates for the info table
+def format_date(myDate):
+    return myDate.strftime("%Y-%m-%d")
+
+def get_today():
+    return format_date(date.today())
+
+def get_tomorrow():
+    tomorrow = date.today() + datetime.timedelta(days=1)
+    return format_date(tomorrow)
+
+# get data from yahoo finance for info table
 yf.pdr_override()  # hijack data from yfinance
-# makes every float in dataframe 2 digits
-pd.options.display.float_format = '{:,.2f}'.format
-
-# get data
-mydata = pdr.get_data_yahoo(yahooStocks, start="2022-01-19", end="2022-01-20")
-df = pd.DataFrame(mydata['Open'])  # get recent quote
-
-# format data
+ydata = pdr.get_data_yahoo(yahooStocks, start=get_today(), end=get_tomorrow())
+df = pd.DataFrame(ydata['Open'])  # get recent quote
 for stock in stocks_test:
-    df[stock] = df[stock].map(lambda x: '{0:.2f}'.format(x))
-
+    df[stock] = df[stock].map(lambda x: '{0:.2f}'.format(x)) # format 2 digits
 df_t = df.T
 df_t.columns = ['Quote']
 df_t.insert(0, "Ticket", stocks_test, True)
 
-print(df_t)
-# dash app
-# X = deque(maxlen = 20)
-# X.append(1)
+# Graph
 
-# Y = deque(maxlen = 20)
-# Y.append(1)
-df_test = px.data.stocks()
 app = dash.Dash(__name__)
 
-# # money = FormatTemplate.money(2)
-
-app.layout = html.Div(
-    [
+app.layout = html.Div([
+    # First Division (Table, Ticker Info)
+    html.Div([
         dash_table.DataTable(
             id='table',
             columns=[{"name": i, "id": i} for i in df_t.columns],
-            data=df_t.to_dict('records')),
-        dcc.Dropdown(
-            id="ticker",
-            options=[{"label": x, "value": x} for x in df_test.columns[1:]],
-            value=df_test.columns[1],
-            clearable=False,
-        ),
-         dcc.Dropdown(
-            id="second_ticker",
-            options=[{"label": x, "value": x} for x in df_test.columns[1:]],
-            value=df_test.columns[1],
-            clearable=False,
-        ),
-        dcc.Graph(id="time-series-chart"),
-    ]
+            data=df_t.to_dict('records')
+        )], style={'padding': 10, 'flex': 1}),
+    # Second Division (Dropdown, Graph)
+    html.Div([
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    id="ticker",
+                    options=[{"label": x, "value": x}
+                             for x in df_t.columns[1:]],
+                    value=df_t.columns[1],
+                    clearable=False,
+
+                )], style={'padding': 10, 'flex': 1},),
+            html.Div([
+                dcc.Dropdown(
+                    id="second_ticker",
+                    options=[{"label": x, "value": x}
+                             for x in df_t.columns[1:]],
+                    value=df_t.columns[1],
+                    clearable=False
+                )], style={'padding': 10, 'flex': 1},),
+        ], style={'display': 'flex', 'flex-direction': 'row'}),
+        html.Div([
+            dcc.Graph(id="time-series-chart")
+        ]),
+    ], style={'padding': 10, 'flex': 1}),
+], style={'display': 'flex', 'flex-direction': 'row'}
 )
 
+# Graph updates (reacts to user choices)
 @app.callback(
-    Output("time-series-chart", "figure"), 
-    [Input("ticker", "value")])
-def display_time_series(ticker):
-    fig = px.line(df_test, x='date', y=ticker)
+    Output("time-series-chart", "figure"),
+    Input("ticker", "value"),
+    Input("second_ticker", "value"),
+)
+def display_hist_data(ticker, second_ticker):
+    fig = px.line(df_t, x='Quote', y=ticker)
+    fig = px.line(df_t, x='Quote', y=second_ticker)
     return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
